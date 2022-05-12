dd1cc93be8fdc24d335d18e8ec9136663fa940a0
AliaaHas <aliaahassan191@gmail.com>
2022-05-12 23:23:47 +0200

    API

---
+++ DTO/LoginDto.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+  
+namespace Day1of_WenApi.DTO
+{
+    public class LoginDto
+    {
+        [Required]
+        public string name { get; set; }
+
+        [Required]
+        public string password { get; set; }
+    }
+}
---
+++ DTO/RegisterDto.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Day1of_WenApi.DTO
+{
+    public class RegisterDto
+    {
+        [Required]
+        public string name { get; set; }
+        [Required]
+        public string password { get; set; }
+        [Required]
+        public string confirmpassword { get; set; }
+
+        [Required]
+        public string email { get; set; }
+    }
+}
